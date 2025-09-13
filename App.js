fd93756a5fc2e5d2dd74beebddafa4df174bc3a3
Karenn <karenggarcia00@gmail.com>
2025-09-12 23:52:35 -0600

    HolaMundo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.holamundo}>Hola Mundo</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eebefaff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  holamundo:{
+    fontSize:50,
+    borderRadius: 30,
+    backgroundColor: '#fff',
+    padding: 15,
   },
 });
