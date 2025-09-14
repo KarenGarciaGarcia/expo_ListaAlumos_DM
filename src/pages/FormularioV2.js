6c35ca9751060d69925783fe778959eb6f81b04c
Karenn <karenggarcia00@gmail.com>
2025-09-14 16:32:29 -0600

    Act19-Card y Formulario-parte 2

--- src/pages/FormularioV2.js
+++ src/pages/FormularioV2.js
@@ -1,20 +1,42 @@
 
 import { SafeAreaView } from "react-native-safe-area-context";
-import {Image, ScrollView,StyleSheet, Platform, Text, StatusBar, View, Dimensions, } from 'react-native';
+import {TouchableOpacity, Image, ScrollView, StyleSheet, Platform, Text, StatusBar, View, Dimensions, } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
-const {width} = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 export default function FormularioV2() {
     return (
 
         <SafeAreaView style={style.mainS}>
-              <ScrollView>
-                <Text style= {style.title}> Formulario V2</Text>
+            <ScrollView>
+                <Text style={style.title}> Formulario V2</Text>
                 <View style={style.container}>
-                    <Image source={{uri:'https://static.vecteezy.com/system/resources/previews/006/631/105/original/portrait-of-brunette-woman-avatar-of-female-person-icon-of-adult-in-flat-style-vector.jpg'}} style={style.avatar}/>
-
+                    <Image source={{ uri: 'https://static.vecteezy.com/system/resources/previews/006/631/105/original/portrait-of-brunette-woman-avatar-of-female-person-icon-of-adult-in-flat-style-vector.jpg' }} style={style.avatar} />
+                    <Text style={style.name}>KAREN GARCIA GARCIA </Text>
+                    <View style={style.infoContainer}>
+                        <Text style={style.label}>Correo</Text>
+                        <Text style={style.info}>garcia@gmail.com</Text>
+                        <Text style={style.label}>Telefono</Text>
+                        <Text style={style.info}>9374797391</Text>
+                        <Text style={style.label}>Carrera</Text>
+                        <Text style={style.info}>Ing Sistemas Computacionales</Text>
+                        <Text style={style.label}>Especialidad</Text>
+                        <Text style={style.info}>Desarrollo de Software</Text>
+                        <Text style={style.label}>No. Control</Text>
+                        <Text style={style.info}>2134679</Text>
+                        <Text style={style.label}>Materia</Text>
+                        <Text style={style.info}>Desarrollo Móvil</Text>
+                        <Text style={style.label}>Grupo</Text>
+                        <Text style={style.info}>7BS</Text>
+                    </View>
+                    {/* Boton Grande con Icono */}
+                    <TouchableOpacity style={style.botonConIcono}>
+                        <MaterialIcons name="save" size={20} color={'#fff'} style={style.iconoIzquierda} />
+                        <Text style={style.textoBoton}> Guardar</Text>
+                    </TouchableOpacity>
                 </View>
-              </ScrollView>
+            </ScrollView>
         </SafeAreaView>
     );
 
@@ -30,29 +52,75 @@ const style = StyleSheet.create({
         fontSize: 22,
         fontWeight: 'bold',
         marginTop: 20,
-        textAlign: 'center'
+        textAlign: 'center',
+        marginBottom:15,
     },
-    container:{
-        width:'100%',
-        backgroundColor:'#f7f7f7ff',
-        borderRadius:15,
-        padding:20,
-        alignItems:'center',
-        shadowColor:'#000',
-        shadowOpacity:0.05,
-        shadowOffset:{width:0, height:4},
-        shadowRadius:10,
-        elevation:5
+    container: {
+        width: '100%',
+        backgroundColor: '#f7f7f7ff',
+        borderRadius: 15,
+        padding: 20,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOpacity: 0.05,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 10,
+        elevation: 5
     },
-    avatar:{
-        width:width * 0.35,
-        height:width*0.35,
-        borderRadius:(width*0.35)/2,
-        marginBottom:20,
-        borderWidth:2,
-        borderColor:'#dddd'
+    avatar: {
+        width: width * 0.35,
+        height: width * 0.35,
+        borderRadius: (width * 0.35) / 2,
+        marginBottom: 20,
+        borderWidth: 2,
+        borderColor: '#dddd'
 
-    }
-    
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 17,
+        marginBottom: 10,
+        borderRadius:20,
+        backgroundColor: '#e79bf1ff',
+        padding:5
+    },
+    infoContainer: {
+        width: '100%',
+        marginBottom: 15,
+    },
+    label: {
+        fontWeight: 'bold',
+        fontSize: 13,
+        marginBottom: 19,
+
+    },
+    info: {
+        fontSize: 13,
+        marginBottom: 10,
+        borderRadius: 15,
+        backgroundColor: '#e5dee6ff',
+        padding: 10
+    },
+    textoBoton:{
+        color:'#ffffffff',
+        fontWeight: 'bold',
+        fontSize:15,
+        textAlign:'center'
+
+    },
+     botonConIcono:{
+        backgroundColor:'#2c67d4ff',
+        marginTop: 10,
+        flexDirection:'row',
+        alignContent:'center',
+        justifyContent:'center',
+        paddingVertical:12,
+        borderRadius:15,
+        width:'40%'
+    },
+    iconoIzquierda:{
+        marginRight:10
+    },
+
 
 })
